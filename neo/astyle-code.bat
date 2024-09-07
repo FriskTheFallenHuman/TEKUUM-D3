7ee70f5af6af6cd4d764b4aa1a32aaafd1529ff4
Krispy <nikodash2020@hotmail.com>
2024-09-07 13:00:21 -0500

    Change GPL back to Doom 3 One

--- neo/astyle-code.bat
+++ neo/astyle-code.bat
@@ -2,6 +2,6 @@
 cls
 
 astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --recursive *.h
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --exclude="game/gamesys/SysCvar.cpp" --exclude="game/gamesys/Callbacks.cpp" --exclude="sys/win32/win_cpu.cpp" --recursive *.cpp
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --exclude="game/gamesys/SysCvar.cpp" --exclude="game/gamesys/Callbacks.cpp" --exclude="d3xp/gamesys/SysCvar.cpp" --exclude="d3xp/gamesys/Callbacks.cpp" --exclude="sys/win32/win_cpu.cpp" --recursive *.cpp
 
 @pause
